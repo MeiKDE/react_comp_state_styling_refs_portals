0a984ec8f147124b64a08d2e1f9447cc9f2a45b8
Mei Zhang <rubymxzhang@gmail.com>
2025-04-19 21:41:40 +0800

    Update project component to handle optional chaining for description and adjust package names in package.json and package-lock.json for consistency.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ export default function Project({ project }) {
     <section className="p-4">
       <div className="flex flex-col p-6 rounded-lg w-full max-w-2xl mx-auto">
         <div className="flex justify-between items-center w-full">
-          <h1 className="text-5xl font-bold">Learning React</h1>
+          <h1 className="text-5xl font-bold">{project?.title}</h1>
           <button className="px-4 py-2 text-xl bg-gray-600 text-white rounded hover:bg-gray-700">
             Delete
           </button>
@@ -13,8 +13,7 @@ export default function Project({ project }) {
       </div>
       <div className="w-full max-w-2xl px-6 mx-auto space-y-4">
         <p className="text-xl text-gray-500">{date.toLocaleDateString()}</p>
-        <p className="text-xl">{project.title}</p>
-        <p className="text-xl">{project.description}</p>
+        <p className="text-xl">{project?.description}</p>
       </div>
     </section>
   );
